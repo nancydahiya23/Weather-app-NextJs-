4b0bd0952c7ef1013506f4a09753bd39ec52f87d
nancydahiya23 <169124678+nancydahiya23@users.noreply.github.com>
2025-09-19 11:21:23 +0530

    Update home.js

--- src/app/city/home.js
+++ src/app/city/home.js
@@ -5,7 +5,7 @@ export default function Return() {
   return (
     <div className='text-center mt-[20px]'>
       <p className="text-gray-300 mb-6 mx-[20px]">
-      We couldn't find any city matching your search.
+      We couldn&apos;t find any city matching your search.
       Please try a different name.
     </p>
         <h3 className='text-2xl'>Go to Home page <Link className="text-blue-400 font-bold hover:text-blue-500 hover:underline transition duration-200" href="/">Home</Link></h3>
